0c975b93c2f6722657bccbbf3dda997470e36afd
ARTTTT-TTTT <tanapat.panmas@gmail.com>
2025-08-28 16:52:57 +0700

    Add unit test for HelloWorld main method to verify console output

--- src/test/java/hello/HelloWorldTest.java
+++ src/test/java/hello/HelloWorldTest.java
@@ -2,6 +2,8 @@ package hello;
 
 import org.junit.Test;
 import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class HelloWorldTest {
     
@@ -15,5 +17,22 @@ public class HelloWorldTest {
     public void testNotNull() {
         HelloWorld hello = new HelloWorld();
         assertNotNull(hello.getMessage());
+    }
+    
+    @Test
+    public void testMain() {
+        // Capture System.out output
+        ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        PrintStream originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+        
+        try {
+            // Test main method
+            HelloWorld.main(new String[]{});
+            assertEquals("Hello World!\n", outContent.toString());
+        } finally {
+            // Restore original System.out
+            System.setOut(originalOut);
+        }
     }
 }
